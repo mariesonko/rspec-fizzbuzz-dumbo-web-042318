f5e5d691845ab604b781677299116833f5a40091
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-17 02:57:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz (int)
 if (int % 3 == 0) && (int % 5 == 0)
-  return "FizzBuzz"
+   "FizzBuzz"
 elsif int % 3 == 0
-  return "Fizz"
+  "Fizz"
 elsif int % 5 ==0
-  return "Buzz"
-else return "nil"
+   "Buzz"
+else "nil"
 end
 end
 
